d6c9a3b0b5ffd3955fa3c514bf83266790ae9a1a
Marvin Arnold <marvinmarnold@gmail.com>
2015-04-09 13:45:22 -0500

    Load user contacts

--- src/main/java/co/gounplugged/unpluggeddroid/application/BaseApplication.java
+++ src/main/java/co/gounplugged/unpluggeddroid/application/BaseApplication.java
@@ -1,6 +1,14 @@
 package co.gounplugged.unpluggeddroid.application;
 
 import android.app.Application;
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.provider.ContactsContract;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import co.gounplugged.unpluggeddroid.api.APICaller;
 import co.gounplugged.unpluggeddroid.db.DatabaseAccess;
@@ -12,10 +20,10 @@ import co.gounplugged.unpluggeddroid.models.Mask;
  * Serves as global application instance
  */
 public class BaseApplication extends Application {
-
+    private static final String TAG = "BaseApplication";
     private APICaller apiCaller;
     private Krewe mKnownMasks;
-
+    private List<Contact> contacts;
 
     /**
      * Get new masks from api or cache on app start
@@ -26,7 +34,7 @@ public class BaseApplication extends Application {
 
         apiCaller = new APICaller(getApplicationContext());
         seedKnownMasks();
-
+        loadContacts();
     }
 
     private void seedKnownMasks() {
@@ -50,4 +58,32 @@ public class BaseApplication extends Application {
         this.mKnownMasks = knownMasks;
     }
 
+    private void loadContacts() {
+        //TODO: prefill from db
+        this.contacts = new ArrayList();
+        ContentResolver cr = getContentResolver();
+        Cursor cur = cr.query(ContactsContract.Contacts.CONTENT_URI,
+                null, null, null, null);
+        if (cur.getCount() > 0) {
+            while (cur.moveToNext()) {
+                String id = cur.getString(cur.getColumnIndex(ContactsContract.Contacts._ID));
+                String name = cur.getString(cur.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+                if (Integer.parseInt(cur.getString(
+                        cur.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER))) > 0) {
+                    Cursor pCur = cr.query(
+                            ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                            null,
+                            ContactsContract.CommonDataKinds.Phone.CONTACT_ID +" = ?",
+                            new String[]{id}, null);
+                    while (pCur.moveToNext()) {
+                        String phoneNo = pCur.getString(pCur.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                        Contact c = new Contact(name, phoneNo, "+1");
+                        contacts.add(c);
+                        Log.d(TAG, "Name: " + name + ", Phone No: " + phoneNo);
+                    }
+                    pCur.close();
+                }
+            }
+        }
+    }
 }
