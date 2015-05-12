ee142d4e8456b2ecd4e163b51d544cd0e635e3ab
Marvin Arnold <marvinmarnold@gmail.com>
2015-05-12 17:36:13 -0500

    Pass tests

--- src/androidTest/java/co/gounplugged/unpluggeddroid/test/models/MaskTest.java
+++ src/androidTest/java/co/gounplugged/unpluggeddroid/test/models/MaskTest.java
@@ -4,6 +4,7 @@ import android.test.AndroidTestCase;
 
 import co.gounplugged.unpluggeddroid.exceptions.InvalidPhoneNumberException;
 import co.gounplugged.unpluggeddroid.models.Mask;
+import co.gounplugged.unpluggeddroid.models.PhoneNumberParser;
 
 /**
  * Created by pili on 9/04/15.
@@ -26,7 +27,7 @@ public class MaskTest extends AndroidTestCase{
 
         public void testSanitize() {
             try {
-                assertEquals("+123456789", Mask.sanitizePhoneNumber("(+)123  --456789  ))"));
+                assertEquals("+123456789", PhoneNumberParser.sanitizePhoneNumber("(+)123  --456789  ))"));
             } catch (InvalidPhoneNumberException e) {
                 assertTrue(false);
             }
@@ -35,11 +36,11 @@ public class MaskTest extends AndroidTestCase{
        public void testParseNumber() {
             String n = "+123123";
             try {
-                assertEquals("+1", Mask.parseCountryCode(n));
-                assertEquals("23123", Mask.parsePhoneNumber(n));
+                assertEquals("+1", PhoneNumberParser.parseCountryCode(n));
+                assertEquals("23123", PhoneNumberParser.parsePhoneNumber(n));
                 n = "+3-21-321";
-                assertEquals("+32", Mask.parseCountryCode(n));
-                assertEquals("1321", Mask.parsePhoneNumber(n));
+                assertEquals("+32", PhoneNumberParser.parseCountryCode(n));
+                assertEquals("1321", PhoneNumberParser.parsePhoneNumber(n));
             } catch (InvalidPhoneNumberException e) {
                 assertTrue(false);
             }
