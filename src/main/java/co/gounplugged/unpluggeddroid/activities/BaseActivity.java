2731aa69a616a4914a1eeabe5e240a40dac4e472
Tim Van Ginderen <timvanginderen@gmail.com>
2015-06-16 17:49:30 +0200

    Use AsyncTask to load contacts and show progress-indicator while syncing

--- src/main/java/co/gounplugged/unpluggeddroid/activities/BaseActivity.java
+++ src/main/java/co/gounplugged/unpluggeddroid/activities/BaseActivity.java
@@ -1,5 +1,7 @@
 package co.gounplugged.unpluggeddroid.activities;
 
+import android.os.AsyncTask;
+import android.os.Build;
 import android.support.v7.app.ActionBarActivity;
 
 public class BaseActivity extends ActionBarActivity {
@@ -12,4 +14,5 @@ public class BaseActivity extends ActionBarActivity {
     protected void displayHomeAsUp() {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+
 }
--- src/main/java/co/gounplugged/unpluggeddroid/fragments/SearchContactFragment.java
+++ src/main/java/co/gounplugged/unpluggeddroid/fragments/SearchContactFragment.java
@@ -1,5 +1,7 @@
 package co.gounplugged.unpluggeddroid.fragments;
 
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -10,10 +12,12 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AutoCompleteTextView;
 import android.widget.ImageButton;
+import android.widget.ProgressBar;
 
 import java.util.List;
 
 import co.gounplugged.unpluggeddroid.R;
+import co.gounplugged.unpluggeddroid.activities.BaseActivity;
 import co.gounplugged.unpluggeddroid.activities.ChatActivity;
 import co.gounplugged.unpluggeddroid.adapters.ContactAdapter;
 import co.gounplugged.unpluggeddroid.events.ConversationEvent;
@@ -26,9 +30,12 @@ import co.gounplugged.unpluggeddroid.utils.ConversationUtil;
 import de.greenrobot.event.EventBus;
 
 public class SearchContactFragment extends Fragment {
+
     private final static String TAG = "SearchContactFragment";
     private AutoCompleteTextView mContactAutoComplete;
     private ImageButton mRefreshContactsButton;
+    private ProgressBar mContactSyncProgressBar;
+    private LoadContactsTask mLoadContactsTask;
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -57,11 +64,24 @@ public class SearchContactFragment extends Fragment {
         mRefreshContactsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ContactUtil.loadContactsInThread(v.getContext());
+                mLoadContactsTask = new LoadContactsTask();
+                if(Build.VERSION.SDK_INT >= 11)
+                    mLoadContactsTask.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+                else
+                    mLoadContactsTask.execute();
             }
         });
 
+        mContactSyncProgressBar = (ProgressBar) view.findViewById(R.id.progress_contacts_sync);
+
         return view;
+    }
+
+    @Override
+    public void onDestroy() {
+        if (mLoadContactsTask.getStatus() == AsyncTask.Status.RUNNING)
+            mLoadContactsTask.cancel(true);
+        super.onDestroy();
     }
 
     private void addConversation(Contact contact) {
@@ -86,6 +106,38 @@ public class SearchContactFragment extends Fragment {
                 ConversationEvent.ConversationEventType.SWITCHED, newConversation);
         EventBus.getDefault().postSticky(event);
     }
+
+    private class LoadContactsTask extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            toggleProgressBar();
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            ContactUtil.loadContacts(getActivity().getApplicationContext());
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            if (!isCancelled())
+                toggleProgressBar();
+        }
+
+
+        private void toggleProgressBar() {
+            if (mContactSyncProgressBar.getVisibility() == View.GONE) {
+                mContactSyncProgressBar.setVisibility(View.VISIBLE);
+                mRefreshContactsButton.setVisibility(View.GONE);
+            } else {
+                mContactSyncProgressBar.setVisibility(View.GONE);
+                mRefreshContactsButton.setVisibility(View.VISIBLE);
+            }
+        }
+    };
 
 
 }
