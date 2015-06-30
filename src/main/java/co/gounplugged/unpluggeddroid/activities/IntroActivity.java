d372f6ae7c82ec1f87c92e0059c4c219b5988123
Tim Van Ginderen <timvanginderen@gmail.com>
2015-06-30 03:45:56 +0200

    Save profile info when clicking next button

--- src/main/java/co/gounplugged/unpluggeddroid/activities/IntroActivity.java
+++ src/main/java/co/gounplugged/unpluggeddroid/activities/IntroActivity.java
@@ -1,7 +1,9 @@
 package co.gounplugged.unpluggeddroid.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -9,6 +11,8 @@ import android.widget.RadioGroup;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import co.gounplugged.unpluggeddroid.R;
+import co.gounplugged.unpluggeddroid.application.BaseApplication;
+import co.gounplugged.unpluggeddroid.models.Profile;
 
 public class IntroActivity extends BaseActivity {
 
@@ -32,6 +36,8 @@ public class IntroActivity extends BaseActivity {
         getSupportActionBar().setTitle(getString(R.string.app_name));
 
         setupRadioGroup();
+        radioGroupSmsPlan.check(0);
+
 
     }
 
@@ -39,6 +45,32 @@ public class IntroActivity extends BaseActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_intro, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.menu_intro_action_next) {
+            saveProfileInfo();
+            BaseApplication.getInstance(this).seedKnownMasks();
+            startMain();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void startMain() {
+        Intent mainIntent = new Intent(this, ChatActivity.class);
+        startActivity(mainIntent);
+        finish();
+    }
+
+    private void saveProfileInfo() {
+        String phoneNumber = editTextPhoneNumber.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+        int smsPlanId = radioGroupSmsPlan.getCheckedRadioButtonId();
+
+        Profile.setPhoneNumber(phoneNumber);
+        Profile.setPassword(password);
+        Profile.setSmsPlan(smsPlanId);
     }
 
     private void setupRadioGroup() {
@@ -51,7 +83,6 @@ public class IntroActivity extends BaseActivity {
             radioButton.setText(option);
             radioGroupSmsPlan.addView(radioButton);
         }
-        radioGroupSmsPlan.check(0);
     }
 
 
