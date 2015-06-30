bde422c7c46bc76376da584305a61c2f560772eb
Tim Van Ginderen <timvanginderen@gmail.com>
2015-06-30 04:40:13 +0200

    Refactor intro-activity in fragments hosted in view-pager

--- src/main/java/co/gounplugged/unpluggeddroid/activities/IntroActivity.java
+++ src/main/java/co/gounplugged/unpluggeddroid/activities/IntroActivity.java
@@ -1,31 +1,28 @@
 package co.gounplugged.unpluggeddroid.activities;
 
-import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.os.Bundle;
-import android.provider.Telephony;
-import android.util.Log;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.ViewPager;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
-import android.widget.TextView;
-import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import co.gounplugged.unpluggeddroid.R;
 import co.gounplugged.unpluggeddroid.application.BaseApplication;
-import co.gounplugged.unpluggeddroid.models.Profile;
+import co.gounplugged.unpluggeddroid.fragments.ProfileIntroFragment;
+import co.gounplugged.unpluggeddroid.fragments.ProvisionIntroFragment;
 
 public class IntroActivity extends BaseActivity {
 
-    @Bind(R.id.et_phone_number) EditText editTextPhoneNumber;
-    @Bind(R.id.et_password) EditText editTextPassword;
-    @Bind(R.id.rg_sms_plan) RadioGroup radioGroupSmsPlan;
-    @Bind(R.id.tv_default_app_status) TextView textViewDefaultAppStatus;
+    @Bind(R.id.viewpager) ViewPager viewPager;
+
+    List<Fragment> mFragments;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,10 +34,7 @@ public class IntroActivity extends BaseActivity {
         setupToolbar();
         getSupportActionBar().setTitle(getString(R.string.app_name));
 
-        setupRadioGroup();
-        radioGroupSmsPlan.check(0);
-
-        setupDefaultAppTextView();
+        setupViewPager();
     }
 
     @Override
@@ -52,11 +46,26 @@ public class IntroActivity extends BaseActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == R.id.menu_intro_action_next) {
-            saveProfileInfo();
-            BaseApplication.getInstance(this).seedKnownMasks();
-            startMain();
+            if(viewPager.getCurrentItem() == 0) {
+                final ProvisionIntroFragment fragment = (ProvisionIntroFragment) mFragments.get(0);
+                if (fragment.isInputValid()) {
+                    viewPager.setCurrentItem(1, true);
+                }
+            } else if (viewPager.getCurrentItem() == 1) {
+                BaseApplication.getInstance(this).seedKnownMasks();
+                startMain();
+            }
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void setupViewPager() {
+        mFragments = new ArrayList<>(2);
+        mFragments.add(ProvisionIntroFragment.newInstance());
+        mFragments.add(ProfileIntroFragment.newInstance());
+
+        final FragmentPagerAdapter adapter = new FragmentPagerAdapter(getSupportFragmentManager(), mFragments);
+        viewPager.setAdapter(adapter);
     }
 
     private void startMain() {
@@ -65,49 +74,23 @@ public class IntroActivity extends BaseActivity {
         finish();
     }
 
-    private void saveProfileInfo() {
-        String phoneNumber = editTextPhoneNumber.getText().toString().trim();
-        String password = editTextPassword.getText().toString().trim();
-        int smsPlanId = radioGroupSmsPlan.getCheckedRadioButtonId();
+    static class FragmentPagerAdapter extends android.support.v4.app.FragmentPagerAdapter {
 
-        Profile.setPhoneNumber(phoneNumber);
-        Profile.setPassword(password);
-        Profile.setSmsPlan(smsPlanId);
-    }
+        private final List<Fragment> mViewPagerFragments;
 
-    private void setupRadioGroup() {
-        final String[] options = getResources().getStringArray(R.array.sms_plans_array);
-
-        int i=0;
-        for (String option : options) {
-            RadioButton radioButton = new RadioButton(this);
-            radioButton.setId(i++);
-            radioButton.setText(option);
-            radioGroupSmsPlan.addView(radioButton);
+        public FragmentPagerAdapter(FragmentManager fm, List<Fragment> fragments) {
+            super(fm);
+            mViewPagerFragments = fragments;
         }
-    }
 
-    private void setupDefaultAppTextView() {
-        if(BaseApplication.getInstance(getApplicationContext()).isDefaultSMSApp()) {
-            textViewDefaultAppStatus.setText("This is the default app");
-        } else {
-            textViewDefaultAppStatus.setText("Not the default app");
-            textViewDefaultAppStatus.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    try {
-                        //http://android-developers.blogspot.com/2013/10/getting-your-sms-apps-ready-for-kitkat.html
-                        Intent intent = new Intent(Telephony.Sms.Intents.ACTION_CHANGE_DEFAULT);
-                        intent.putExtra(Telephony.Sms.Intents.EXTRA_PACKAGE_NAME, getApplicationContext().getPackageName());
-                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                        getApplicationContext().startActivity(intent);
-                    } catch (ActivityNotFoundException e ) {
-                        //This shouldn't even happen, this app should be installed on sms compatible devices only
-                        Toast.makeText(getApplicationContext(), "No default sms selection available.", Toast.LENGTH_LONG).show();
-                    }
+        @Override
+        public Fragment getItem(int index) {
+            return mViewPagerFragments.get(index);
+        }
 
-                }
-            });
+        @Override
+        public int getCount() {
+            return mViewPagerFragments.size();
         }
     }
 
